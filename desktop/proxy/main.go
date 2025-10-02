876cfa5eb6b33e58a0c0b1d0a47bcff7c88f50ce
Pouria <pouriashaigani@gmail.com>
2025-10-02 22:33:20 +0330

    Update projec without dependencies on the original intra for test

--- desktop/proxy/main.go
+++ desktop/proxy/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-    "context"
+    "bytes"
     "flag"
+    "fmt"
+    "io"
     "net"
+    "net/http"
     "os"
     "os/signal"
     "syscall"
     "time"
 
-    "github.com/Jigsaw-Code/Intra/go/intra"
     "github.com/miekg/dns"
     "github.com/sirupsen/logrus"
 )
@@ -17,12 +19,10 @@ import (
 type DNSProxy struct {
     dohURL  string
     server  *dns.Server
-    client  *intra.DoHClient
-    cache   *intra.SimpleCache
+    client  *http.Client
+    cache   map[string]*dns.Msg
     log     *logrus.Logger
-    metrics struct {
-        queries, cacheHits, failures int
-    }
+    metrics struct { queries, cacheHits, failures int }
 }
 
 func NewDNSProxy(host, port, dohURL string) *DNSProxy {
@@ -31,8 +31,8 @@ func NewDNSProxy(host, port, dohURL string) *DNSProxy {
     return &DNSProxy{
         dohURL: dohURL,
         server: &dns.Server{Addr: net.JoinHostPort(host, port), Net: "udp"},
-        client: intra.NewDoHClient(dohURL),
-        cache:  intra.NewSimpleCache(100),
+        client: &http.Client{Timeout: 5 * time.Second},
+        cache:  make(map[string]*dns.Msg),
         log:    log,
     }
 }
@@ -46,39 +46,123 @@ func (p *DNSProxy) Start() error {
 func (p *DNSProxy) handleDNS(w dns.ResponseWriter, r *dns.Msg) {
     p.metrics.queries++
     key := r.String()
-    if resp, ok := p.cache.Get(key); ok {
+    if resp, ok := p.cache[key]; ok {
         p.metrics.cacheHits++
         p.log.Debug("Cache hit")
         w.WriteMsg(resp)
         return
     }
 
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    resp, err := p.client.Query(ctx, r)
+    resp, err := p.queryDoH(r)
     if err != nil {
         p.metrics.failures++
         p.log.Errorf("DoH error: %v", err)
-        w.WriteMsg(&dns.Msg{Response: true, Rcode: dns.RcodeServerFailure})
+        msg := new(dns.Msg)
+        msg.SetReply(r)
+        msg.Rcode = dns.RcodeServerFailure
+        w.WriteMsg(msg)
         return
     }
 
-    ttl := time.Duration(resp.Answer[0].Header().Ttl) * time.Second
-    p.cache.Set(key, resp, ttl)
+    ttl := 300 * time.Second
+    if len(resp.Answer) > 0 {
+        ttl = time.Duration(resp.Answer[0].Header().Ttl) * time.Second
+    }
+    p.cache[key] = resp
+    go func() {
+        time.Sleep(ttl)
+        delete(p.cache, key)
+    }()
     p.log.Infof("Resolved query: %s", r.Question[0].Name)
     w.WriteMsg(resp)
 }
 
+// func (p *DNSProxy) queryDoH(r *dns.Msg) (*dns.Msg, error) {
+//     buf, err := r.Pack()
+//     if err != nil {
+//         return nil, err
+//     }
+//     req, err := http.NewRequest("POST", p.dohURL, bytes.NewReader(buf))
+//     if err != nil {
+//         return nil, err
+//     }
+//     req.Header.Set("Content-Type", "application/dns-message")
+//     req.Header.Set("Accept", "application/dns-message")
+
+//     resp, err := p.client.Do(req)
+//     if err != nil {
+//         return nil, err
+//     }
+//     defer resp.Body.Close()
+
+//     if resp.StatusCode != http.StatusOK {
+//         return nil, fmt.Errorf("DoH request failed: %s", resp.Status)
+//     }
+
+//     body, err := io.ReadAll(resp.Body)
+//     if err != nil {
+//         return nil, err
+//     }
+
+//     msg := &dns.Msg{}
+//     if err := msg.Unpack(body); err != nil {
+//         return nil, err
+//     }
+//     return msg, nil
+// }
+func (p *DNSProxy) queryDoH(r *dns.Msg) (*dns.Msg, error) {
+    buf, err := r.Pack()
+    if err != nil {
+        p.log.Errorf("Failed to pack DNS message: %v", err)
+        return nil, err
+    }
+    req, err := http.NewRequest("POST", p.dohURL, bytes.NewReader(buf))
+    if err != nil {
+        p.log.Errorf("Failed to create HTTP request: %v", err)
+        return nil, err
+    }
+    req.Header.Set("Content-Type", "application/dns-message")
+    req.Header.Set("Accept", "application/dns-message")
+
+    resp, err := p.client.Do(req)
+    if err != nil {
+        p.log.Errorf("HTTP request failed: %v", err)
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        p.log.Errorf("DoH request failed: %s", resp.Status)
+        return nil, fmt.Errorf("DoH request failed: %s", resp.Status)
+    }
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        p.log.Errorf("Failed to read response body: %v", err)
+        return nil, err
+    }
+
+    msg := &dns.Msg{}
+    if err := msg.Unpack(body); err != nil {
+        p.log.Errorf("Failed to unpack DNS response: %v", err)
+        return nil, err
+    }
+    return msg, nil
+}
+
 func (p *DNSProxy) Shutdown() {
-    p.log.Infof("Shutting down. Metrics: queries=%d, cacheHits=%d, failures=%d",
-        p.metrics.queries, p.metrics.cacheHits, p.metrics.failures)
+    p.log.Infof("Shutting down. Metrics: queries=%d, cacheHits=%d, failures=%d", p.metrics.queries, p.metrics.cacheHits, p.metrics.failures)
     p.server.Shutdown()
 }
 
 func main() {
+    // host := flag.String("host", "127.0.0.1", "Listen host")
+    // port := flag.String("port", "5353", "Listen port")
+    // dohURL := flag.String("doh", "https://cloudflare-dns.com/dns-query", "DoH server URL")
+    // flag.Parse()
     host := flag.String("host", "127.0.0.1", "Listen host")
     port := flag.String("port", "5353", "Listen port")
-    dohURL := flag.String("doh", "https://cloudflare-dns.com/dns-query", "DoH server URL")
+    dohURL := flag.String("doh", "https://dns.google/dns-query", "DoH server URL")
     flag.Parse()
 
     proxy := NewDNSProxy(*host, *port, *dohURL)
